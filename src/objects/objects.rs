12c427df9e205ba8278c8de636d9b2b6906031fd
myselfBZ <boburforfun@gmail.com>
2026-06-13 17:03:26 +0500

    env added

--- src/objects/objects.rs
+++ src/objects/objects.rs
@@ -1,6 +1,32 @@
 use core::fmt;
+use std::{collections::HashMap, iter::Map, ptr::hash};
 
 
+pub struct Env {
+    store: HashMap<String, Object>
+}
+
+
+impl Env {
+    pub fn new() -> Env {
+        return Env{
+            store: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, key: String) -> Option<Object> {
+        match self.store.get(&key) {
+            Some(v) => return Some(v.clone()),
+            None => return None
+        }
+    }
+
+    pub fn set(&mut self, key: String, val: &Object) {
+        self.store.insert(key, val.clone());
+    }
+}
+
+#[derive(Clone)]
 pub enum Object {
     Int(i32),
     Bool(bool),
